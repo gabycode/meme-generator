e882ba2cd5aa1183af5e50151274bc732178ad25
gabycode <86427446+gabycode@users.noreply.github.com>
2022-10-28 21:39:25 -0400

    Create App.js

---
+++ meme-generator/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Meme from './components/Meme';
+
+function App() {
+  return (
+    <div className="main">
+      <Header/>
+      <Meme/>
+    </div>
+  );
+}
+
+export default App;
